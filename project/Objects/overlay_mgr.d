2ccb4a1643872d5c1195a934e26a75233f754e40
Librianets <librianets@gmail.com>
2022-08-22 04:44:51 +0700

    restruct

--- project/Objects/overlay_mgr.d
+++
@@ -1,6 +0,0 @@
-.\objects\overlay_mgr.o: ..\..\..\src\mcu\rtl876x\overlay_mgr.c
-.\objects\overlay_mgr.o: ..\..\..\src\app\pxp\app_flags.h
-.\objects\overlay_mgr.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\overlay_mgr.o: ..\..\..\inc\platform\overlay_mgr.h
-.\objects\overlay_mgr.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\overlay_mgr.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
--- project/Objects/startup_rtl8762c.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_rtl8762c.o: ..\..\..\src\mcu\rtl876x\arm\startup_rtl8762c.s
