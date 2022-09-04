c1df8b06940ae2f25d3bde31d3d6ebef662dfff5
Librianets <librianets@gmail.com>
2022-09-05 03:02:05 +0700

    demo 0.3

--- demoproject_1/keil/after_build_common.bat
+++ demoproject_1/keil/after_build_common.bat
@@ -3,7 +3,6 @@ set keil_compiler_include_floder=%1
 set full_name_path=%2
 set linker_output_file_name=%3
 set fromelf_bin=app
-
 echo keil_compiler_include_floder = %keil_compiler_include_floder%
 echo full_name_path = %full_name_path%
 echo linker_output_file_name = %linker_output_file_name%
@@ -11,15 +10,19 @@ echo fromelf_bin = %fromelf_bin%
 
 %keil_compiler_include_floder%\..\bin\fromelf.exe --bin --output=.\bin\ "%full_name_path%"
 echo fromelf1
+
 %keil_compiler_include_floder%\..\bin\fromelf.exe -acd --interleave=source -o "bin\%linker_output_file_name%.disasm" "%full_name_path%"
 echo fromelf2
-..\tool\prepend_header\prepend_header.exe -t app_code -p "bin\%fromelf_bin%.bin" -m 1 -c sha256 -a "..\..\..\tool\key.json"
+ren .\bin\%fromelf_bin%.bin %linker_output_file_name%.bin
+ren .\bin\%fromelf_bin%.trace %linker_output_file_name%.trace
+echo ren
+..\tool\prepend_header\prepend_header.exe -t app_code -p "bin\%linker_output_file_name%.bin" -m 1 -c sha256 -a "..\..\..\tool\key.json"
 echo prepend_header
-..\tool\MD5\md5.exe "bin\%fromelf_bin%.bin"
+..\tool\MD5\md5.exe "bin\%linker_output_file_name%.bin"
 echo md51
-..\tool\MD5\md5.exe "bin\%fromelf_bin%_MP.bin"
+..\tool\MD5\md5.exe "bin\%linker_output_file_name%_MP.bin"
 echo md52
-del "bin\%fromelf_bin%_MP.bin"
+del "bin\%linker_output_file_name%_MP.bin"
 echo del
 
 
